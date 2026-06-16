e1f45e5ad42ed8be6e0845f7cb8193d11f4eb328
Ruben Torrejon Espinosa <ruben.torrejon@securitasdirect.es>
2026-06-16 11:34:27 +0200

    Primer commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Tocame la minga dominga que viene de Francia");
+        System.out.println("Holi mundo");
     }
 }
